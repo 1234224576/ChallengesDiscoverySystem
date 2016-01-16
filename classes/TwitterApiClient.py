1662e841630d17e086841e380b1971097a7dfccc
曽和修平 <sowashuhei@gmail.com>
2016-01-16 16:13:29 +0900

    TwitterAPIClientを作成

---
+++ classes/TwitterApiClient.py
@@ -0,0 +1,38 @@
+# --- coding: utf-8 ---
+from requests_oauthlib import OAuth1Session
+import json
+
+class TwitterApiClient(object):
+	"""Connect by TwitterAPI"""
+
+	def __init__(self, consumerKey,consumerSecret,accessToken,accessTokenSecret):
+		self.consumerKey = consumerKey
+		self.consumerSecret = consumerSecret
+		self.accessToken = accessToken
+		self.accessTokenSecret  = accessTokenSecret
+
+
+	def create_oath_session(self):
+		oath = OAuth1Session(self.consumerKey,self.consumerSecret,self.accessToken,self.accessTokenSecret)
+		return oath
+
+	def search(self,keyword):
+		url = "https://api.twitter.com/1.1/search/tweets.json"
+		params = {
+        "q": unicode(keyword),
+        "lang": "ja",
+        "result_type": "recent",
+        "count": "100"
+        }
+		oath = self.create_oath_session()
+		responce = oath.get(url, params = params)
+		print(responce.status_code)
+		if responce.status_code != 200:
+			print "Error code: %d" %(responce.status_code)
+			return None
+		tweets = json.loads(responce.text)
+
+		for tweet in tweets["statuses"]:
+			text = tweet[u'text']
+			print "text:", text
+		return tweets
